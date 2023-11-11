74099975c96d40208d6e7931634d275f248d0b9b
nani-samireddy <nanisamireddy05@gmail.com>
2023-11-11 15:49:13 +0530

    added cors

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use("/api/songs", songRoutes);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://adonai-backend.vercel.app"
+  );
 
   res.setHeader(
     "Access-Control-Allow-Methods",
